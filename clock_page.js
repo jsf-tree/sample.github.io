b4e685ab55f89fe3d016d0ea03fd9bce097716a8
jsf-tree <juliano.finck@codex.com.br>
2023-06-08 11:25:07 -0300

    version 2 - responsive

--- clock_page.js
+++ clock_page.js
@@ -1,65 +1,99 @@
 function updateClock() {
-    const now = new Date();
-    const clockText = document.querySelector("h2#clock_text");
-    let hour = now.getHours();
-    let min = now.getMinutes();
-    let sec = now.getSeconds();
+    const actualTime = document.querySelector("input#wrong_clock").value;
+    const clockText = document.querySelector("h2#clock_text");    
+    if (actualTime == '') {
+        const now = new Date();
+        let hour = now.getHours();
+        let min = now.getMinutes();
+        let sec = now.getSeconds();
 
-    function addLeadingZero(n) {
-        let nOut = n.toString();
-        if (nOut.length < 2) {
-            return '0' + nOut;
-        } else {
-            return nOut;
+        function addLeadingZero(n) {
+            let nOut = n.toString();
+            if (nOut.length < 2) {
+                return '0' + nOut;
+            } else {
+                return nOut;
+            }
         }
+        hour = addLeadingZero(hour)
+        min = addLeadingZero(min)
+        sec = addLeadingZero(sec)
+        clockText.innerHTML = `${hour}:${min}:${sec}`;
+    } else {
+        clockText.innerHTML = `${actualTime.toString()}:00`
     }
-
-    hour = addLeadingZero(hour)
-    min = addLeadingZero(min)
-    sec = addLeadingZero(sec)
-
-    clock_text.innerHTML = `${hour}:${min}:${sec}`;
+    
 }
 
-updateClock();
-
-setInterval(updateClock, 1000);
-
-function load() {
+function updateSectionHeight() {
     // Update container height according to its width
     const container = document.querySelector('section');
     const width = getComputedStyle(container).width;
     const numericWidth = parseFloat(width.match(/\d+/)[0]);
-    container.style.height = `${numericWidth/1.618}px` //1.618
-
-    // Color
-    const now = new Date();
-    const body = document.querySelector('body')
-
-    const daytime = Number(now.getHours()) + Number(now.getMinutes())/60
-    const text = document.querySelector("div#time_msg")
-    const img = document.querySelector("div.image-container")
-    // pexels - Free and Open Image Database
-    if (daytime >= 7 && daytime < 11) {
-        text.innerHTML = "It's morning"
-        body.style.background = "rgb(255, 253, 116)";
-        img.innerHTML = '<img src="img/morning.jpg" alt="morning">'
-    } else if (daytime >= 11 && daytime < 13.5) {
-        text.innerHTML = "It's lunch time"
-        body.style.background = "rgb(255, 238, 0)";
-        img.innerHTML = '<img src="img/lunch.jpg" alt="lunch">'
-    } else if (daytime >= 13.5 && daytime < 17) {
-        text.innerHTML = "It's afternoon"
-        body.style.background = "rgb(255, 162, 0)";
-        img.innerHTML = '<img src="https://www.civitatis.com/f/kenia/nairobi/free-tour-nairobi-r32.jpg" alt="afternoon">'
-
-    } else if (daytime >= 17 && daytime < 19) {
-        text.innerHTML = "It's sunset time"
-        body.style.background = "rgb(255, 34, 0)";
-        img.innerHTML = '<img src="img/sunset.jpg" alt="Nairobi Free Tour">'
-    } else {
-        text.innerHTML = "It's night"
-        body.style.background = "rgb(22, 0, 31)";
-        img.innerHTML = '<img src="img/night.jpg" alt="night">'
-    }
+    container.style.height = `${numericWidth}px` //1.618
 }
+
+
+function updateTimeStyle () {
+    function imgsrc(a_string) {
+        return `<img src="img/${a_string}.png" alt="${a_string}">`
+    }
+
+    function setStyle(period_of_the_day) {
+        // pexels - Free and Open Image Database
+        const periodsOfTheDay = {
+            'morning': {
+                'msg': "It's morning", 
+                'img': imgsrc('morning')},
+            'lunch': {
+                'msg': "It's lunch time",
+                'img': imgsrc('lunch')},
+            'afternoon': {
+                'msg': "It's afternoon",
+                'img': imgsrc('afternoon')},
+            'sunset': {
+                'msg': "It's sunset",
+                'img': imgsrc('sunset')},
+            'night': {
+                'msg': "It's night",
+                'img': imgsrc('night')},
+        }
+        const text = document.querySelector("div#time_msg")
+        const img = document.querySelector("div.large")
+        const body = document.querySelector('body')
+        text.innerHTML = periodsOfTheDay[period_of_the_day].msg
+        body.classList.add(period_of_the_day);
+        console.log(body.classList)
+        img.innerHTML = periodsOfTheDay[period_of_the_day].img
+    }
+
+    const clockText = document.querySelector("h2#clock_text").innerText; 
+    const timeParts = clockText.split(':');
+
+    const h = parseInt(timeParts[0]);
+    const m = parseInt(timeParts[1]);
+    const s = parseInt(timeParts[2]);
+    const daytime = h + m/60 + s/3600;
+    
+    if (daytime >= 7 && daytime < 11) {
+        setStyle('morning')
+    } else if (daytime >= 11 && daytime < 13.5) {
+        setStyle('lunch')
+    } else if (daytime >= 13.5 && daytime < 17) {
+        setStyle('afternoon')
+    } else if (daytime >= 17 && daytime < 19) {
+        setStyle('sunset')
+    } else {
+        setStyle('night')
+    }
+
+}
+
+function load() {
+    updateClock();
+    updateTimeStyle();
+}
+
+//updateSectionHeight();
+load();
+setInterval(load, 1000);
